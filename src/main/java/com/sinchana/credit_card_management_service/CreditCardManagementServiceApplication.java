03cfcc53ddf00776e51fd3cb056fa8d500203706
Sinchana_Rokkad <143598377+sinchanarokkad@users.noreply.github.com>
2025-09-24 21:02:34 +0530

    added CreditCardManagementSericeApplication

---
+++ src/main/java/com/sinchana/credit_card_management_service/CreditCardManagementServiceApplication.java
@@ -0,0 +1,11 @@
+package com.sinchana.credit_card_management_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditCardManagementServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CreditCardManagementServiceApplication.class, args);
+    }
+}
