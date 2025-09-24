6d6f5daaf092130ff49cc454b7303277d6f29de0
Sinchana_Rokkad <143598377+sinchanarokkad@users.noreply.github.com>
2025-09-24 21:06:13 +0530

    test

---
+++ src/test/java/com/sinchana/credit_card_management_service/CreditCardManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sinchana.credit_card_management_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditCardManagementServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+        // just checks if Spring context loads
+    }
+}
